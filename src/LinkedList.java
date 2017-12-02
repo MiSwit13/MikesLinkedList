3b962866c9e6cbe2f0a4d3eaae416151ea1b92f1
miswit13 <Aud13rey!>
2017-12-02 02:13:41 -0800

    fixed node names

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -2,10 +2,9 @@ public class LinkedList {
     public LinkedListNode head = null;
 
     public void add(LinkedListNode node) {
-        if(head == null) {
+        if (head == null) {
             head = node;
-        }
-        else {
+        } else {
             node.next = head;
             head = node;
         }
--- src/MikesLinkedListApp.java
+++ src/MikesLinkedListApp.java
@@ -3,8 +3,8 @@ public class MikesLinkedListApp {
     public static void main(String[] args) {
         LinkedList ll = new LinkedList(); //creates a new list called "ll"
 
-        LinkedListNode node = new LinkedListNode(5); //creates a new node called "node" with a value of 5
-        ll.add(node); //adds "node" to the linked list "ll"
+        LinkedListNode node1 = new LinkedListNode(5); //creates a new node called "node1" with a value of 5
+        ll.add(node1); //adds "node" to the linked list "ll"
 
         LinkedListNode node2 = new LinkedListNode(7);
         ll.add(node2);
@@ -12,7 +12,7 @@ public class MikesLinkedListApp {
         LinkedListNode node3 = new LinkedListNode(9);
         ll.add(node3);
 
-        System.out.println("The value of node 1 is " + node.value);
+        System.out.println("The value of node 1 is " + node1.value);
         System.out.println("The value of node 2 is " + node2.value);
         System.out.println("The value of node 3 is " + node3.value);
     }
