6f473faab32381c40a39be43552c77f002393459
Michael Switzer <stun13@gmail.com>
2019-04-06 15:08:02 -0700

    Initial commit

---
+++ course-reviews-api/src/main/java/com/solitarymonk/course/Course.java
@@ -0,0 +1,39 @@
+package com.solitarymonk.course;
+
+import com.solitarymonk.core.BaseEntity;
+
+import javax.persistence.Entity;
+
+
+@Entity
+public class Course extends BaseEntity {
+
+    private String title;
+    private String url;
+
+    protected Course() {
+        super();
+    }
+
+    public Course(String title, String url) {
+        this();
+        this.title = title;
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ course-reviews-api/src/main/java/com/solitarymonk/review/ReviewRepository.java
@@ -0,0 +1,6 @@
+package com.solitarymonk.review;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReviewRepository extends CrudRepository<Review, Long> {
+}
