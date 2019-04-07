1b3c20a9bb12d238d8788568a1140540d9f1bc92
Michael Switzer <stun13@gmail.com>
2019-04-07 12:37:24 -0700

    Commit cleanup

--- course-reviews-api/src/main/java/com/solitarymonk/course/Course.java
+++ course-reviews-api/src/main/java/com/solitarymonk/course/Course.java
@@ -1,24 +1,45 @@
 package com.solitarymonk.course;
 
 import com.solitarymonk.core.BaseEntity;
+import com.solitarymonk.review.Review;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
 public class Course extends BaseEntity {
-
+    @NotNull
+    @Size(min = 2, max = 140)
     private String title;
     private String url;
 
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
+    private List<Review> reviews;
+
     protected Course() {
         super();
+        reviews = new ArrayList<>();
     }
 
     public Course(String title, String url) {
         this();
         this.title = title;
         this.url = url;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void addReview(Review review) {
+        review.setCourse(this);
+        reviews.add(review);
     }
 
     public String getTitle() {
--- course-reviews-api/src/main/java/com/solitarymonk/review/Review.java
+++ course-reviews-api/src/main/java/com/solitarymonk/review/Review.java
@@ -1,17 +1,37 @@
 package com.solitarymonk.review;
 
 import com.solitarymonk.core.BaseEntity;
+import com.solitarymonk.course.Course;
+import com.solitarymonk.user.User;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 
 @Entity
 public class Review extends BaseEntity {
     private  int rating;
     private String description;
+    @ManyToOne
+    private Course course;
+    @ManyToOne
+    private User reviewer;
     
     protected Review() {
         super();
+    }
+
+    public Review(int rating, String description) {
+        this.rating = rating;
+        this.description = description;
+    }
+
+    public User getReviewer() {
+        return reviewer;
+    }
+
+    public void setReviewer(User reviewer) {
+        this.reviewer = reviewer;
     }
 
     public int getRating() {
@@ -28,5 +48,13 @@ public class Review extends BaseEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
     }
 }
